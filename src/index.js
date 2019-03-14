41c5a6bd8a70e7a6ef05cfbfeafeffb765d47d45
Ante Penić <antepenic94@gmail.com>
2019-03-14 15:28:26 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Clock from './Clock';
+import App from './components/App';
+import Clock from './components/Clock';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
