77b8b68570cb78141a0a8238857f1a78a057dcbc
Ante Penić <antepenic94@gmail.com>
2019-03-14 13:27:34 +0100

    update

--- src/index.js
+++ src/index.js
@@ -4,21 +4,5 @@ import App from './App';
 import Clock from './Clock';
 import './index.css';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
-}
-  
-const user = {
-    firstName: 'Ante',
-    lastName: 'Penic'
-};
-  
-const element = (
-    <h3>
-        Hello, {formatName(user)}!
-    </h3>
-);
-  
-ReactDOM.render(element, document.getElementById('user'));
 ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<Clock />, document.getElementById('clock'));
